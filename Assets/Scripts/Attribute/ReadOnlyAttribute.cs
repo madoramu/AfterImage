d6af36b645d7160c91efc08398f993b1c3c03060
madoramu <sn13sknkb@yahoo.co.jp>
2020-12-20 12:53:37 +0900

    残像用マテリアル追加。斧の残像Prefab追加。Inspector上で編集不可状態にする属性を追加

---
+++ Assets/Scripts/Attribute/ReadOnlyAttribute.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Inspector上で編集不可にする属性
+/// </summary>
+public class ReadOnlyAttribute : PropertyAttribute{ }
+
+#if UNITY_EDITOR
+/// <summary>
+/// ReadOnly属性を付けた時のInspector上の描画を設定するクラス
+/// </summary>
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyDrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return EditorGUI.GetPropertyHeight(property, label, true);
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = true;
+    }
+}
+
+#endif
