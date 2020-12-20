0d109d859e3e76f5e6c7ea8df0dc657af2ae5202
madoramu <sn13sknkb@yahoo.co.jp>
2020-12-20 15:21:39 +0900

    残像用コンポーネント作成。マテリアルの動的生成の為ShaderGraphを元にマテリアルを作成+MeshRendererに設定するように変更

---
+++ Assets/Scripts/AfterImage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// 残像用オブジェクトにアタッチするコンポーネント
+/// </summary>
+public class AfterImage : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer _meshRenderer = null;
+    [SerializeField] private Shader _shader = null;
+    [SerializeField, Range(0f, 1f)] private float _rate = 0f;
+    [SerializeField] private Gradient _gradient = null;
+    [SerializeField, ReadOnly] private Material _material = null;
+
+    private int _baseColorID = -1;
+
+    private void Awake()
+    {
+        if (_meshRenderer == null)
+        {
+            throw new Exception("Mesh Renderer null");
+        }
+
+        // シェーダーを元にマテリアルを作成
+        _baseColorID = Shader.PropertyToID("_BaseColor");
+        _material = new Material(_shader);
+
+        _meshRenderer.material = _material;
+        UpdateColor();
+    }
+
+    private void Update()
+    {
+        UpdateColor();
+    }
+
+    private void UpdateColor()
+    {
+        _material.SetColor(_baseColorID, _gradient.Evaluate(_rate));
+    }
+}
